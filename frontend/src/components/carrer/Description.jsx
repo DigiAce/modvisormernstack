490b92bf20ccd704c2cd76c0557e4978e59488df
plex-op <karthikkn877@gmail.com>
2025-03-01 22:37:42 +0530

    mt for section

--- frontend/src/components/carrer/Description.jsx
+++ frontend/src/components/carrer/Description.jsx
@@ -117,7 +117,7 @@ const Discription = () => {
 
       {showForm && (
         <div className="fixed inset-0 flex justify-center items-center p-10 mt-32 sm:mt-20">
-          <div className="bg-white p-8 rounded-2xl shadow-lg max-w-lg w-full relative">
+          <div className="bg-white p-8 rounded-2xl shadow-lg max-w-lg w-full relative mt-10">
             <button
               className="absolute top-4 right-4 text-gray-400 hover:text-gray-600"
               onClick={toggleForm}
